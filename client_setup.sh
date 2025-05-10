2772c23417bbcf7019a741f20405537af912c594
Jehoon Song <song.jehoon@gmail.com>
2025-05-10 11:29:38 +0900

    .

---
+++ client_setup.sh
@@ -0,0 +1,7 @@
+# Ubuntu 기준 설치
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+apt update
+apt install -y nvidia-docker2
+systemctl restart docker
